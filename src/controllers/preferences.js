86f060716b71c760c8f59e4020112969a2694d11
Matthew Hummer <biggreyson@gmail.com>
2021-11-16 21:41:12 -0700

    feat(#39): add stub functions to preference controller

--- src/controllers/preferences.js
+++ src/controllers/preferences.js
@@ -1,24 +1,58 @@
+const Group = require('../models/Group');
+const Preference = require('../models/Preference');
+
 const PreferencesController = {
-  // index: (req, res, next) => {
+  // index: async (req, res, next) => {
   //   return res.send(`GET /groups/${req.params.groupId}/preferences [index]`);
   // },
-  // show: (req, res, next) => {
+  // show: async (req, res, next) => {
   //   return res.send(`GET /groups/${req.params.groupId}/preferences/${req.params.preferenceId} [show]`);
   // },
-  // create: (req, res, next) => {
+  // create: async (req, res, next) => {
   //   return res.send(`POST /groups/${req.params.groupId}/preferences [create]`);
   // },
-  // new: (req, res, next) => {
+  // new: async (req, res, next) => {
   //   return res.send(`GET /groups/${req.params.groupId}/new [new]`);
   // },
-  update: (req, res, next) => {
-    return res.send(`POST /groups/${req.params.groupId}/preferences [update]`);
+  update: async (req, res, next) => {
+    // We do a sort of upsert here.
+    const group = await Group.findById(req.params.groupId);
+    let preference = await Preference.findOne({
+      group,
+      user: req.user,
+    });
+    if (!preference) {
+      // TODO: refactor this into som sort of upsert logic
+      preference = await Preference.create({
+        group,
+        user: req.user,
+      });
+    }
+    // TODO: update emails and phoneNumbers; not sure what body is yet
+    await preference.save();
+    req.flash('success', 'You Preferences for this group have been successfully updated.');
+
+    return res.redirect('/');
   },
-  edit: (req, res, next) => {
-    return res.send(`GET /groups/${req.params.groupId}/preferences [edit]`);
+  edit: async (req, res, next) => {
+    let preference = await Preference.findOne({
+      group,
+      user: req.user,
+    });
+    if (!preference) {
+      // TODO: refactor this into som sort of upsert logic
+      preference = await Preference.create({
+        group,
+        user: req.user,
+      });
+    }
+
+    return res.render('preferences/form', {
+      preference,
+    });
   },
   // },
-  // destroy: (req, res, next) => {
+  // destroy: async (req, res, next) => {
   //   return res.send(`DELETE /groups/${req.params.groupId}/preferences/${req.params.preferenceId} [destroy]`);
   // },
 };
