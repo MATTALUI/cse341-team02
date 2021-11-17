166540c98afc9be676c537b3815b3b450899e993
Matthew Hummer <biggreyson@gmail.com>
2021-11-16 21:21:54 -0700

    feat(#39): add controller stub functions for messages

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,26 +1,44 @@
+const Group = require('../models/Group');
+const Message = require('../models/Message');
+
 const MessagesController = {
-  // index: (req, res, next) => {
+  // index: async (req, res, next) => {
   //   return res.send(`GET /groups/${req.params.groupId}/messages [index]`);
   // },
-  show: (req, res, next) => {
-    return res.send(`GET /groups/${req.params.groupId}/messages/${req.params.messageId} [show]`);
-  },
-  create: (req, res, next) => {
-    return res.send(`POST /groups/${req.params.groupId}/messages [create]`);
-  },
-  new: (req, res, next) => {
-    res.render('common/groupMessageCreation', {
-      csrfToken: req.csrfToken(),
-      currentUser: req.user,
+  show: async (req, res, next) => {
+    const message = await Message.findById(req.params.messageId);
+
+    return render('messages/show', {
+      message,
     });
   },
-  // update: (req, res, next) => {
+  create: async (req, res, next) => {
+    const group = await Group.findById(req.params.groupId);
+    const message = await Message.create({
+      ...req.body,
+      group,
+      poster: req.user,
+    });
+    // TODO: send the message out to everyone in the group
+    req.flash('success', `Your message has been posted.`);
+
+    return res.redirect('/messages');
+  },
+  new: async (req, res, next) => {
+    const message = new Message();
+
+    return res.render('common/groupMessageCreation', {
+      message,
+      csrfToken: req.csrfToken(),
+    });
+  },
+  // update: async (req, res, next) => {
   //   return res.send(`POST /groups/${req.params.groupId}/messages/${req.params.messageId} [update]`);
   // },
-  // edit: (req, res, next) => {
+  // edit: async (req, res, next) => {
   //   return res.send(`GET /groups/${req.params.groupId}/messages/${req.params.messageId}/edit [edit]`);
   // },
-  // destroy: (req, res, next) => {
+  // destroy: async (req, res, next) => {
   //   return res.send(`DELETE /groups/${req.params.groupId}/messages/${req.params.messageId} [destroy]`);
   // },
 };
