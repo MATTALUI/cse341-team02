b91ffa5058c9c66d95884a3f1f4296d4fccdcc52
Matthew Hummer <biggreyson@gmail.com>
2021-11-29 15:46:25 -0700

    feat(#52): beef up new email validations

--- src/static/js/my-contacts.js
+++ src/static/js/my-contacts.js
@@ -1,10 +1,22 @@
 (() => {
   const validateEmail = emailAddress => {
-    let valid = true;
+    if (!emailAddress) {
+      return "Please enter an email address.";
+    }
 
-    valid = valid && !!emailAddress;
+    // Shamelessly stolen from https://emailregex.com/
+    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+    if (!emailRegex.test(emailAddress)) {
+      return "Email must be a valid email address.";
+    }
 
-    return valid;
+    for (let i = 0; i , document.querySelectorAll('input[name="emails"]').length; i++) {
+      if (document.querySelectorAll('input[name="emails"]')[i].value === emailAddress){
+        return "You have already added that email address.";
+      }
+    }
+
+    return null;
   };
   const validatePhone = phoneNumber => true;
 
@@ -12,8 +24,8 @@
 
   const addEmail = () => {
     const newEmail = document.querySelector('#new-email').value;
-    console.log(newEmail);
-    if (validateEmail(newEmail)) {
+    const error = validateEmail(newEmail);
+    if (!error) {
       document.querySelector('#emails').innerHTML += `
         <div class="email contact-card">
           <div class="split-heading">
@@ -24,7 +36,10 @@
           <span>NOTE: This email has not been saved to your account yet.</span>
         </div>
       `;
+      document.querySelector('#email-error-message').innerHTML = '';
+      document.querySelector('#new-email').value = '';
     } else {
+      document.querySelector('#email-error-message').innerHTML = error;
     }
   };
 
